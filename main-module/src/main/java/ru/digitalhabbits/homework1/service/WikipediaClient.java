ebb2fb8cc8731f7d8625983e4f96bbf3d6810296
brikki <igor.brikkel@gmail.com>
2020-10-11 13:03:49 +0300

    WikiClient done

--- main-module/src/main/java/ru/digitalhabbits/homework1/service/WikipediaClient.java
+++ main-module/src/main/java/ru/digitalhabbits/homework1/service/WikipediaClient.java
@@ -1,10 +1,23 @@
 package ru.digitalhabbits.homework1.service;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WikipediaClient {
     public static final String WIKIPEDIA_SEARCH_URL = "https://en.wikipedia.org/w/api.php";
@@ -12,8 +25,29 @@ public class WikipediaClient {
     @Nonnull
     public String search(@Nonnull String searchString) {
         final URI uri = prepareSearchUrl(searchString);
-        // TODO: NotImplemented
-        return "";
+        String result = "";
+
+        try (final CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            final Gson gson = new Gson();
+            final HttpGet request = new HttpGet(uri);
+
+
+            final CloseableHttpResponse response = httpClient.execute(request);
+            final String entity = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+            final JsonObject json = gson.fromJson(entity, JsonObject.class)
+                    .getAsJsonObject("query")
+                    .getAsJsonObject("pages");
+            final List<String> keySet = new ArrayList<>(json.keySet());
+            //System.out.println(json.getAsJsonObject(keySet.get(0))
+//                    .getAsJsonPrimitive("extract")
+//                    .getAsString());
+            return json.getAsJsonObject(keySet.get(0))
+                     .getAsJsonPrimitive("extract")
+                    .getAsString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Nonnull
